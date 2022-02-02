0d0f6dbd8e632a08fd9fee5ae170d722bcc4243b
delta-kor <deltakbot1@gmail.com>
2022-02-02 16:37:29 +0900

    Fix manifest

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,3 +1,11 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import isCrawler from './services/crawl';
+
+if (!isCrawler()) {
+  // @ts-ignore
+  precacheAndRoute(window.self.__WB_MANIFEST || []);
+}
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
