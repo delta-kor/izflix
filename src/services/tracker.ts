e9841a570a4b640be96db9e7144be8df4945f95e
delta-kor <deltakbot1@gmail.com>
2022-02-12 13:42:01 +0900

    Fix params

--- src/services/tracker.ts
+++ src/services/tracker.ts
@@ -2,7 +2,12 @@ import ReactGA from 'react-ga4';
 
 class Tracker {
   public static send(action: string, params: any = {}): void {
-    ReactGA.event({ category: 'event', action, ...params });
+    ReactGA.ga('send', {
+      hitType: 'event',
+      eventCategory: 'event',
+      eventAction: action,
+      ...params,
+    });
   }
 }
 
