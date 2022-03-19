21563f727d6421296d997e208e35032360bd90f4
delta-kor <deltakbot1@gmail.com>
2022-03-19 23:01:37 +0900

    Fix service worker

--- public/sw.js
+++ public/sw.js
@@ -13,12 +13,16 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', function (event) {
+  const url = event.request.url;
+  const urls = ['cdn.jsdelivr.net', '.css', '.js'];
+  if(!urls.some(target => url.includes(target))) return;
+
   event.respondWith(
     (async function () {
       try {
         const response = await fetch(event.request);
         const type = response.headers.get('content-type');
-        const cacheType = ['text/html', 'font/woff', 'text/css'];
+        const cacheType = ['text/html', 'font/woff', 'text/css', 'application/javascript'];
         const isCacheType = type && cacheType.some((value) => type.includes(value));
         
         if(response.status === 200 && isCacheType) {
