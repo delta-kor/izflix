4550e3dd46558d29646495a21f5860c2330a4cb8
delta-kor <deltakbot1@gmail.com>
2022-05-14 21:23:56 +0900

    Add user connection

--- src/components/pages/LivePage.tsx
+++ src/components/pages/LivePage.tsx
@@ -2,18 +2,58 @@ import { motion } from 'framer-motion';
 import { Component } from 'react';
 import styled from 'styled-components';
 import Pingpong from '../../services/pingpong';
+import LiveHeader from '../menus/LiveHeader';
 
 const Page = styled(motion.main)`
   text-align: center;
 `;
 
-const pingpong = new Pingpong(process.env.REACT_APP_SOCKET_BASE!);
+interface State {
+  users: Map<string, UserInfo>;
+  liveUsers: Map<string, UserInfo>;
+}
 
-class LivePage extends Component {
+class LivePage extends Component<any, State> {
+  pingpong!: Pingpong;
+
+  state: State = {
+    users: new Map(),
+    liveUsers: new Map(),
+  };
+
+  componentDidMount = () => {
+    this.pingpong = new Pingpong(process.env.REACT_APP_SOCKET_BASE!);
+    this.mountEventListeners();
+  };
+
+  mountEventListeners = () => {
+    this.pingpong.on('usersync', (infos) => {
+      this.addUser(...infos);
+    });
+    this.pingpong.on('userconnect', (...infos) => {
+      this.addUser(...infos);
+
+      const liveUsers = this.state.liveUsers;
+      for (const info of infos) liveUsers.set(info.id, info);
+      this.setState({ liveUsers });
+    });
+    this.pingpong.on('userdisconnect', (...ids) => {
+      const liveUsers = this.state.liveUsers;
+      for (const id of ids) liveUsers.delete(id);
+      this.setState({ liveUsers });
+    });
+  };
+
+  addUser = (...infos: UserInfo[]) => {
+    const users = this.state.users;
+    for (const info of infos) users.set(info.id, info);
+    this.setState({ users });
+  };
+
   render() {
     return (
       <Page>
-        <h1>live</h1>
+        <LiveHeader liveCount={this.state.liveUsers.size} />
       </Page>
     );
   }
--- src/services/pingpong.ts
+++ src/services/pingpong.ts
@@ -1,3 +1,4 @@
+import { EventEmitter } from 'events';
 import Settings from './settings';
 import getTicket from './ticket';
 import Transmitter from './transmitter';
@@ -8,12 +9,31 @@ interface PacketPromise {
   reject: any;
 }
 
-class Pingpong {
+interface PingpongEvents {
+  me(id: string): void;
+  usersync(infos: UserInfo[]): void;
+  userconnect(...infos: UserInfo[]): void;
+  userdisconnect(...ids: string[]): void;
+}
+
+declare interface Pingpong {
+  on<U extends keyof PingpongEvents>(
+    event: U,
+    listener: PingpongEvents[U]
+  ): this;
+  emit<U extends keyof PingpongEvents>(
+    event: U,
+    ...args: Parameters<PingpongEvents[U]>
+  ): boolean;
+}
+
+class Pingpong extends EventEmitter {
   private socket!: WebSocket;
   private packetId: number = 1;
   private promises: PacketPromise[] = [];
 
   constructor(url: string) {
+    super();
     try {
       this.socket = new WebSocket(url);
       this.socket.addEventListener('open', this.onOpen.bind(this));
@@ -25,50 +45,6 @@ class Pingpong {
       console.error(e);
       this.onError('서버 연결에 실패했어요\n다시 시도해 주세요');
     }
-  }
-
-  private async onOpen(): Promise<void> {
-    try {
-      const ticket = await getTicket();
-      const token = Settings.getOne('$_LIVE_TOKEN');
-
-      const helloPacket: ClientPacket.Hello = {
-        packet_id: 0,
-        type: 'hello',
-        ticket,
-        token,
-      };
-      const response = await this.sendPacketAndWait<ServerPacket.Hello>(
-        helloPacket
-      );
-
-      const newToken = response.token;
-      Settings.setOne('$_LIVE_TOKEN', newToken);
-    } catch (e: any) {
-      console.error(e);
-      this.onError(e.message);
-    }
-  }
-
-  private onMessage(event: MessageEvent): void {
-    try {
-      const data = JSON.parse(event.data) as ServerPacketBase;
-      for (const resolve of this.promises) {
-        if (resolve.packetId === data.packet_id) {
-          if (data.type === 'error') resolve.reject(data);
-          else resolve.resolve(data);
-        }
-      }
-    } catch (e) {
-      console.error(e);
-      this.onError();
-    }
-  }
-
-  private onError(
-    message: string = '오류가 발생했어요\n다시 시도해 주세요'
-  ): void {
-    Transmitter.emit('popup', message);
   }
 
   private sendPacketAndWait<T extends ServerPacketBase>(
@@ -92,6 +68,82 @@ class Pingpong {
 
     const json = JSON.stringify(packet);
     this.socket.send(json);
+  }
+
+  private onError(
+    message: string = '오류가 발생했어요\n다시 시도해 주세요'
+  ): void {
+    Transmitter.emit('popup', message);
+  }
+
+  private async onOpen(): Promise<void> {
+    try {
+      const ticket = await getTicket();
+      const token = Settings.getOne('$_LIVE_TOKEN');
+
+      const helloPacket: ClientPacket.Hello = {
+        packet_id: 0,
+        type: 'hello',
+        ticket,
+        token,
+      };
+      const response = await this.sendPacketAndWait<ServerPacket.Hello>(
+        helloPacket
+      );
+
+      const newToken = response.token;
+      Settings.setOne('$_LIVE_TOKEN', newToken);
+
+      this.emit('me', response.user_info.id);
+    } catch (e: any) {
+      console.error(e);
+      this.onError(e.message);
+    }
+  }
+
+  private onMessage(event: MessageEvent): void {
+    try {
+      const data = JSON.parse(event.data) as ServerPacketBase;
+      for (const resolve of this.promises) {
+        if (resolve.packetId === data.packet_id) {
+          if (data.type === 'error') resolve.reject(data);
+          else resolve.resolve(data);
+        }
+      }
+
+      switch (data.type) {
+        case 'user-sync':
+          this.onUserSync(data as any);
+          break;
+        case 'user-connect':
+          this.onUserConnect(data as any);
+          break;
+        case 'user-disconnect':
+          this.onUserDisconnect(data as any);
+          break;
+      }
+    } catch (e) {
+      console.error(e);
+      this.onError();
+    }
+  }
+
+  private onUserSync(packet: ServerPacket.UserSync): void {
+    const users = packet.data;
+    this.emit('usersync', users);
+    if (packet.packet_id === null) {
+      this.emit('userconnect', ...users);
+    }
+  }
+
+  private onUserConnect(packet: ServerPacket.UserConnect): void {
+    const user = packet.user_info;
+    this.emit('userconnect', user);
+  }
+
+  private onUserDisconnect(packet: ServerPacket.UserDisconnect): void {
+    const id = packet.id;
+    this.emit('userdisconnect', id);
   }
 }
 
