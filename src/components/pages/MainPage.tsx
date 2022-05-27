1c085171dfc0fc6c547436a1a7ead5ce34720ae0
delta-kor <deltakbot1@gmail.com>
2022-05-27 18:00:24 +0900

    Add link menu

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import { Component } from 'react';
 import styled from 'styled-components';
 import Constants from '../../constants';
 import Scroll from '../../services/scroll';
-import { MobileQuery } from '../../styles';
+import { Color, MobileQuery } from '../../styles';
 import LandingVideo from '../actions/LandingVideo';
 import PlaylistMenu from '../menus/PlaylistMenu';
 import Meta from '../Meta';
@@ -13,6 +13,31 @@ const Page = styled(motion.main)`
   ${MobileQuery} {
     padding: 0 0 64px 0;
   }
+`;
+
+const LinkMenu = styled.a`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+  width: calc(100vw - 64px);
+  max-width: 720px;
+  margin: 8px auto;
+  padding: 16px 16px;
+  background: ${Color.DARK_GRAY};
+  border-radius: 6px;
+`;
+
+const LinkMenuTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  color: ${Color.WHITE};
+`;
+
+const LinkMenuDescription = styled.div`
+  font-size: 14px;
+  font-weight: normal;
+  color: ${Color.WHITE};
+  opacity: 0.7;
 `;
 
 class MainPage extends Component {
@@ -40,6 +65,10 @@ class MainPage extends Component {
         <Mobile>
           <LandingVideo />
         </Mobile>
+        <LinkMenu href={'https://fanchant.izflix.net'}>
+          <LinkMenuTitle>아이즈원 응원법 연습</LinkMenuTitle>
+          <LinkMenuDescription>https://fanchant.izflix.net</LinkMenuDescription>
+        </LinkMenu>
         <PlaylistMenu />
       </Page>
     );
