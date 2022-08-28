8531824baf97dcb7783009bfa5b354de26d721c9
delta-kor <deltakbot1@gmail.com>
2022-08-28 18:18:15 +0900

    Add panorama state loader

--- src/hooks/usePanorama.tsx
+++ src/hooks/usePanorama.tsx
@@ -13,6 +13,7 @@ interface Panorama extends PanoramaMethods {
 function usePanorama(): Panorama {
   const [videoInfo, setVideoInfo] = useState<ApiResponse.Video.Info | undefined>();
   const [streamInfo, setStreamInfo] = useState<ApiResponse.Video.Stream | undefined>();
+  const [videoList, setVideoList] = useState<IVideo[]>([]);
 
   const view = async (id: string, state?: VideoPageState) => {
     setVideoInfo(undefined);
@@ -26,7 +27,33 @@ function usePanorama(): Panorama {
     return response;
   };
 
-  const loadState = async (state: VideoPageState) => {};
+  const loadState = async (state: VideoPageState) => {
+    switch (state.key) {
+      case 'playlist': {
+        const response = await Spaceship.readPlaylist(state.value);
+        if (!response.ok) return false;
+
+        setVideoList(response.playlist.video);
+        break;
+      }
+
+      case 'category': {
+        const response = await Spaceship.viewCategory(state.value);
+        if (!response.ok) return false;
+
+        if (response.type === 'file') setVideoList(response.data);
+        break;
+      }
+
+      case 'calendar': {
+        const response = await Spaceship.getOneCalendar(state.value);
+        if (!response.ok) return false;
+
+        setVideoList(response.videos);
+        break;
+      }
+    }
+  };
 
   const methods = { view };
 
