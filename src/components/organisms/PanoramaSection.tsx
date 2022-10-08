dc51c12c22da998a8b8eb219be700d7362a7ba23
delta-kor <deltakbot1@gmail.com>
2022-10-08 22:06:07 +0900

    Add panorama content area

--- src/components/organisms/PanoramaSection.tsx
+++ src/components/organisms/PanoramaSection.tsx
@@ -2,27 +2,33 @@ import { motion } from 'framer-motion';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import { Panorama, PanoramaState } from '../../hooks/usePanorama';
-import { Color, MobileQuery, PcInnerPadding, PcQuery } from '../../styles';
+import Icon from '../../icons/Icon';
+import { Color, HideOverflow, MobileQuery, PcInnerPadding, PcQuery, Text } from '../../styles';
+import SmoothBox from '../atoms/SmoothBox';
 
 const RenderArea = styled(motion.div)<{ $state: PanoramaState }>`
-  background: ${Color.DARK_GRAY}AA;
+  background: ${Color.DARK_GRAY}EA;
   z-index: 50;
 
   ${MobileQuery} {
     position: fixed;
-    width: 100%;
 
     ${({ $state }) =>
       $state === PanoramaState.ACTIVE
         ? `
         top: 0;
         left: 0;
+        width: 100%;
         aspect-ratio: 16 / 9;
         `
         : `
         display: flex;
+        left: 16px;
+        right: 16px;
         bottom: 85px;
         height: 86px;
+        border-radius: 8px;
+        overflow: hidden;
         `}
   }
 
@@ -42,6 +48,8 @@ const RenderArea = styled(motion.div)<{ $state: PanoramaState }>`
         right: 24px;
         width: 340px;
         min-height: 20px;
+        border-radius: 8px;
+        overflow: hidden;
         `}
   }
 `;
@@ -58,15 +66,89 @@ const Video = styled.video`
 `;
 
 const VideoArea = styled.div<{ $state: PanoramaState }>`
+  flex-shrink: 0;
+
   ${MobileQuery} {
     height: 100%;
-    aspect-ratio: ${({ $state }) => ($state === PanoramaState.ACTIVE ? '16 / 9' : '18 / 9')};
+    width: ${({ $state }) => ($state === PanoramaState.ACTIVE ? '100%' : '30vw')};
+  }
+
+  ${PcQuery} {
+    width: 100%;
+    aspect-ratio: 16 / 9;
   }
 `;
 
 const ContentArea = styled.div`
   display: flex;
   width: 100%;
+  flex-grow: 1;
+  align-items: center;
+  min-width: 0;
+
+  ${MobileQuery} {
+    padding: 0 16px;
+  }
+
+  ${PcQuery} {
+    padding: 16px 20px;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  gap: 4px;
+  min-width: 0;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  color: ${Color.WHITE};
+  ${HideOverflow};
+
+  ${MobileQuery} {
+    ${Text.SUBTITLE_1};
+  }
+
+  ${PcQuery} {
+    ${Text.EX_SUBTITLE_1};
+  }
+`;
+
+const Description = styled.div`
+  width: 100%;
+  color: ${Color.WHITE};
+  opacity: 0.7;
+  ${HideOverflow};
+
+  ${MobileQuery} {
+    ${Text.SUBTITLE_2};
+  }
+
+  ${PcQuery} {
+    ${Text.SUBTITLE_1};
+  }
+`;
+
+const Icons = styled.div`
+  display: flex;
+  flex-shrink: 0;
+  gap: 10px;
+`;
+
+const IconWrapper = styled(SmoothBox)`
+  width: 26px;
+  height: 26px;
+  padding: 3px;
+  cursor: pointer;
+`;
+
+const MenuIcon = styled(Icon)`
+  flex-shrink: 0;
+  width: 20px;
+  height: 20px;
 `;
 
 interface Props {
@@ -82,8 +164,23 @@ const PanoramaSection: React.FC<Props> = ({ panorama }) => {
 
   const Component = (
     <RenderArea $state={panoramaState}>
-      {panoramaState === PanoramaState.BACKGROUND && <ContentArea></ContentArea>}
       <VideoArea $state={panoramaState}>{video}</VideoArea>
+      {panoramaState === PanoramaState.BACKGROUND && (
+        <ContentArea>
+          <Content>
+            <Title>{panorama.videoInfo?.title}</Title>
+            <Description>{panorama.videoInfo?.description}</Description>
+          </Content>
+          <Icons>
+            <IconWrapper hover={1.1} tap={0.9}>
+              <MenuIcon type={'pause'} color={Color.WHITE} />
+            </IconWrapper>
+            <IconWrapper hover={1.1} tap={0.9}>
+              <MenuIcon type={'close'} color={Color.WHITE} />
+            </IconWrapper>
+          </Icons>
+        </ContentArea>
+      )}
     </RenderArea>
   );
 
