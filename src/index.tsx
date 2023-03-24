39586b9571078fed259d481bc97b66342dff3a54
dtxrital <deltakbot1@gmail.com>
2023-03-25 01:20:28 +0900

    Update tracing rate

--- src/index.tsx
+++ src/index.tsx
@@ -22,9 +22,9 @@ if (Tracker.isActivated()) {
       new BrowserTracing(),
       new Sentry.Replay({ maskAllText: false, blockAllMedia: false }),
     ],
-    tracesSampleRate: 0.5,
-    replaysSessionSampleRate: 0.05,
-    replaysOnErrorSampleRate: 0.1,
+    tracesSampleRate: 0.2,
+    replaysSessionSampleRate: 0.01,
+    replaysOnErrorSampleRate: 0.05,
   });
   console.log('Sentry initialized');
 }
