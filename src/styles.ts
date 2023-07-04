5605926ce2f5e4e56201f21f23b461e4f5bc15df
delta-kor <contact@izflix.net>
2023-07-04 23:08:14 +0900

    Add hyewon theme

--- src/styles.ts
+++ src/styles.ts
@@ -14,7 +14,7 @@ const PcLeftMargin = 112;
 
 const Color = {
   WHITE: '#FFFFFF',
-  PRIMARY: '#549ED8',
+  PRIMARY: '#EC635D',
   GRAY: '#454B6B',
   DARK_GRAY: '#161A36',
   BACKGROUND: '#070D2D',
